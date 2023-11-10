5b358b741eaace4195e6a97e96ca0a093d7672e1
Brian Torres <brian.al.torres@gmail.com>
2023-11-09 19:46:56 -0500

    added proportions

--- backend/sckgs_backend/builders/stix_builder.py
+++ backend/sckgs_backend/builders/stix_builder.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractclassmethod
 
 class StixBuilder(ABC):
     @abstractclassmethod
-    def create(self, data):
+    def create(self):
         raise NotImplementedError("The create() method must be implemented in child classes.")
